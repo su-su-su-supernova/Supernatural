1c265c199d4e35c831f3b5dc6ae33c123e8ade8e
DESKTOP-14S36GK\MSI <reppy0813@gmail.com>
2025-03-19 21:13:08 +0900

    ui틀 완성

---
+++ Source/Supernatural/Public/MainBoardWidget.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ProductInfoWidget.h"
+#include "MainBoardWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SUPERNATURAL_API UMainBoardWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	UMainBoardWidget(const FObjectInitializer& ObjectInitializer);
+
+
+public:
+	virtual void NativeConstruct() override;
+
+
+
+
+
+public:
+	class UProductInfoWidget* ProductInfoWidget;
+
+		UPROPERTY(meta = (BindWidget))
+	class UWrapBox* WrapBox;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Widget")
+	TSubclassOf<class UProductInfoWidget> ProductInfoWidgetTool;
+
+};
