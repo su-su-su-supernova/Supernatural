f4ac1512829abfa3bca47927832169347d08e52f
DESKTOP-14S36GK\MSI <reppy0813@gmail.com>
2025-03-19 21:33:04 +0900

    ProductDataTable2

---
+++ Source/Supernatural/Private/CComputer.cpp
@@ -0,0 +1,20 @@
+#include "CComputer.h"
+
+ACComputer::ACComputer()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ACComputer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACComputer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Supernatural/Public/CPlayer.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CPlayer.generated.h"
+
+UCLASS()
+class SUPERNATURAL_API ACPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACPlayer();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+public:
+	/* Camera */
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent* PlayerCamer;
+
+	/* Inputs */
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputMappingContext* IMC_PlayerInput;
+
+	// Move
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_PlayerMove;
+	void Move(const struct FInputActionValue& InValues);
+
+	// Turn
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_PlayerTurn;
+	void Turn(const struct FInputActionValue& InValues);
+};
