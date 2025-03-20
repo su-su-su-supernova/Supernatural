e27a7e10926752d46c87961e1e8b01612c43592e
D0YUN <191944956+D0YUN@users.noreply.github.com>
2025-03-20 22:04:34 +0900

    [DYL] Product Data Table 진행중

--- Source/Supernatural/Private/SuperGameMode.cpp
+++ Source/Supernatural/Private/SuperGameMode.cpp
@@ -1,35 +1,102 @@
 #include "SuperGameMode.h"
 #include "CProductDataTable.h"
 #include "CProductDataAsset.h"
+#include "EProductType.h"
 
 void ASuperGameMode::BeginPlay()
 {
 	Super::BeginPlay();
-	LoadCSVData(FProductData::StaticStruct());
+	//LoadCSVData(FProductData::StaticStruct());
+	UE_LOG(LogTemp, Error, TEXT(">> SuperBeginPlay"));
+	//CreateProductDataAsset();
 }
 
+// CSV 파일에 있는 데이터로 Data Table 생성
 void ASuperGameMode::LoadCSVData(UScriptStruct* InStruct)
 {
 	FString csvFilePath, csvFileData;
 
 	// 파일 경로 지정
 	csvFilePath = FPaths::ProjectContentDir() / TEXT("DYL/DataTables/DT_ProductDataTable.csv");
+	
 
-	// 파일명을 바탕으로 파일 데이터를 불러오는데 성공하면
+	// 파일명을 바탕으로 CSV 파일 데이터를 불러오는데 성공하면
 	if (FFileHelper::LoadFileToString(csvFileData, *csvFilePath))
 	{
 		// Data Table의 Row Struct를 정의한다
 		DTProduct->RowStruct = InStruct;
 
 		// 파일 데이터를 바탕으로 Data Table을 생성한다
-		TArray<FString> errorMessage = DTProduct->CreateTableFromCSVString(csvFileData);
+		TArray<FString> errorMessages = DTProduct->CreateTableFromCSVString(csvFileData);
+
+		// Data Table 생성에 실패하면
+		if (errorMessages.Num() > 0)
+		{
+			for(const FString err : errorMessages)
+				UE_LOG(LogTemp, Error, TEXT(">> Faild to load Data Table : %s"), *err);
+		}
+		// Data Table 생성에 성공하면
+		else
+		{
+			UE_LOG(LogTemp, Log, TEXT(">> Succes to load Data Table"));
+
+			// Product Data Asset을 생성한다
+			CreateProductDataAsset(DTProduct);
+		}
 	}
+	// CSV 파일 데이터를 불러오는 데 실패했다면
+	else
+		UE_LOG(LogTemp, Error, TEXT(">> Faild to load CSV file from path: %s"), *csvFilePath);
 }
 
-void ASuperGameMode::CreateDataAsset(UDataTable* InDT)
-{
-	// 데이터 테이블의 데이터를 로드한다
+//void ASuperGameMode::CreateProductDataAsset(UDataTable* InDT)
+//{
+//	// InDT에 data가 있다면
+//	if (!InDT)
+//	{
+//		UE_LOG(LogTemp, Warning, TEXT(">> Data Table is null"));
+//		return;
+//	}
+//
+//	// enum 정보 가져오기
+//	UEnum* enumData = StaticEnum<EProductType>();
+//	if (!enumData)
+//	{
+//		UE_LOG(LogTemp, Error, TEXT(">> Failed to find EProductType enum"));
+//		return;
+//	}
+//
+//	int32 productNum = static_cast<int32>(EProductType::MAX);
+//
+//	// 데이터 테이블의 데이터를 로드한다
+//	for (int i = 0; i < productNum; i++)
+//	{
+//		// 1. 행의 이름(Key)를 가져온다
+//		EProductType type = static_cast<EProductType>(i);
+//		FName rowName = FName(enumData->GetNameStringByIndex(i));
+//
+//		// 2. 행의 데이터를 가져온다
+//		FProductData* rowData = InDT->FindRow<FProductData>(rowName,"");
+//
+//		if (!rowData)
+//		{
+//			UE_LOG(LogTemp, Warning, TEXT(">> Row %s not found in Data Table"), *rowName.ToString());
+//			continue;
+//		}
+//
+//		// 3. Product Data Asset 객체를 생성한다
+//		UCProductDataAsset* newProductDataAsset = NewObject<UCProductDataAsset>(this, UCProductDataAsset::StaticClass());
+//		if (!newProductDataAsset)
+//		{
+//			UE_LOG(LogTemp, Error, TEXT("Failed to create Data Asset for %s!"), *rowName.ToString());
+//			continue;
+//		}
+//
+//		// 4. Data Asset의 Product 맵에 데이터를 추가한다
+//		newProductDataAsset->Product.Add(type, *rowData);
+//
+//		UE_LOG(LogTemp, Log, TEXT("Added %s to Product Data Asset."), *rowName.ToString());
+//	}
+//}
 
-	// 
-}
 
--- Source/Supernatural/Public/SuperGameMode.h
+++ Source/Supernatural/Public/SuperGameMode.h
@@ -1,14 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "SuperGameMode.generated.h"
 
-/**
- * 
- */
+class CProductDataTable;
+
 UCLASS()
 class SUPERNATURAL_API ASuperGameMode : public AGameModeBase
 {
@@ -19,11 +16,15 @@ protected:
 
 	/* Product Data Table & Data Asset */
 public:
-	UPROPERTY()
+	UPROPERTY(EditAnywhere, Category = "DataTable")
 	class UDataTable* DTProduct;
-	UPROPERTY()
+
+	UPROPERTY(EditAnywhere, Category = "DataAsset")
 	class UCProductDataAsset* DAProduct;
 
 	void LoadCSVData(UScriptStruct* InStruct);
-	void CreateDataAsset(UDataTable* InDT);
+
+	// void CreateProductDataAsset(UDataTable* InDT);
+
+	void 
 };
