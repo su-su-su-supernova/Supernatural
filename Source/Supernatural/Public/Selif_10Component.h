b7efb41a64a5622d87cbefb45dcaf3c665dbba6e
DESKTOP-14S36GK\MSI <reppy0813@gmail.com>
2025-03-22 17:36:54 +0900

    추가 커밋

---
+++ Source/Supernatural/Public/Selif_10Component.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Selif_BaseComponent.h"
+
+#include "Selif_10Component.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SUPERNATURAL_API USelif_10Component : public USelif_BaseComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USelif_10Component();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+};
